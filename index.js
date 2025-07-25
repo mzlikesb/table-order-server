5236fab317ee288b7b36c7781c58e2b23f01913e
mzlikesb <mintzzzzz42@gmail.com>
2025-07-25 10:30:24 +0900

    Remove commented-out Socket.IO event emissions from index.js for cleaner code and maintainability.

--- index.js
+++ index.js
@@ -38,30 +38,22 @@ io.on('connection', (socket) => {
   
 });
 
-// 주문 관련
-io.to('staff').emit('order-updated', orderData);
-
-// 호출 관련  
-io.to('staff').emit('call-updated', callData);
-
-// 메뉴 관련
-io.to('staff').emit('menu-updated', menuData);
-io.to('table-' + tableId).emit('menu-updated', menuData);
-
-// 테이블 관련
-io.to('staff').emit('table-updated', tableData);
+// Socket.IO 이벤트 예시 (실제 사용 시에는 각 라우트에서 정의된 데이터를 사용)
+// io.to('staff').emit('order-updated', orderData);
+// io.to('staff').emit('call-updated', callData);
+// io.to('staff').emit('menu-updated', menuData);
+// io.to('table-' + tableId).emit('menu-updated', menuData);
+// io.to('staff').emit('table-updated', tableData);
 
 app.set('io', io);
 
 const menusRouter = require('./routes/menus');
-const menuCategoriesRouter = require('./routes/menu-categories');
 const ordersRouter = require('./routes/orders');
 const tablesRouter = require('./routes/tables');
 const callsRouter = require('./routes/calls');
 const storesRouter = require('./routes/stores');
 
 app.use('/api/menus', menusRouter);
-app.use('/api/menu-categories', menuCategoriesRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/tables', tablesRouter);
 app.use('/api/calls', callsRouter);
